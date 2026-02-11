80087176cf43c495f4acaccaed76e620d302f67f
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2026-02-12 00:19:20 +0530

    created routes

---
+++ server/src/routes/rent.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { createRent, getAllRents, getRentById, updateRent, deleteRent } from "../controllers/rent.controller";
+
+const router = Router();
+
+router.post("/", authMiddleware, createRent);
+
+router.get("/", getAllRents);
+
+router.get("/:id", getRentById);
+
+router.patch("/:id", authMiddleware, updateRent);
+
+router.delete("/:id", authMiddleware, deleteRent);
+
+export default router;
