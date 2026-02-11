bce01d72773bbfcc869593cf56515ab1737693f2
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2026-02-11 06:20:31 +0530

    applied jwt authentication and created login, register, profile routes-controllers-services

---
+++ server/src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import authRoutes from './routes/auth.routes'
+import userRoutes from './routes/user.routes'
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+
+export default app;
---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { UserProfile } from "../services/user.service";
+
+export const profile = async (req: Request, res: Response) => {
+  try {
+    const user = await UserProfile(req.body);
+
+  res.status(200).json({
+  message: "User Profile Successfully Fetched",
+  user: {
+    id: user._id,
+    name: user.name,
+    rollNumber: user.rollNumber,
+    phoneNumber: user.phoneNumber,
+    email: user.email,
+    role: user.role
+  }
+});
+
+  } catch (error: any) {
+    res.status(400).json({ message: error.message });
+  }
+};
