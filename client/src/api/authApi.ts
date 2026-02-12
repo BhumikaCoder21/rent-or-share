ff92f56222e401afbf8230e532a1871dbb007568
BhumikaCoder21 <bhumikanew21@gmail.com>
2026-02-12 23:38:49 +0530

    Created api

---
+++ client/src/api/authApi.ts
@@ -0,0 +1,18 @@
+import API from "./axios";
+import {
+  LoginRequest,
+  RegisterRequest,
+  AuthResponse,
+} from "@/types/auth.types";
+
+export const loginUser = async (data: LoginRequest): Promise<AuthResponse> => {
+  const response = await API.post("/auth/login", data);
+  return response.data;
+};
+
+export const registerUser = async (
+  data: RegisterRequest,
+): Promise<AuthResponse> => {
+  const response = await API.post("/auth/register", data);
+  return response.data;
+};
