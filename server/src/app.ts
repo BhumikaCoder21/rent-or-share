b486e961561dc00042a6fffc9553a179a09fae9b
Kaushal Kumar <kaushal.cse.23@nitap.ac.in>
2026-02-15 12:23:37 +0530

    refactor: update vehicle and user routes for improved functionality

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,15 @@
 import express from "express";
-import authRoutes from './routes/auth.routes'
-import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes';
+import userRoutes from './routes/user.routes';
+import vehicleRoutes from './routes/rent.routes';
+import rideRoute from './routes/ride.routes'
 
 const app = express();
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
+app.use("/api/vehicle", vehicleRoutes);
+app.use("/api/ride", rideRoute);
 
 export default app;
--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,19 +1,17 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
-import { profile, getAllProfiles, getProfileById, deleteAllProfiles, deleteProfileById  } from "../controllers/user.controller";
+import {getProfileById, getAllProfiles,deleteAllProfiles, deleteProfileById, updateProfileById  } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/profile", authMiddleware, profile);
+router.get("/profile", authMiddleware, getProfileById);
 
 router.get("/all-profile", authMiddleware, getAllProfiles);
-
-router.get("/profile/:id", authMiddleware, getProfileById);
 
 router.delete("/profiles", authMiddleware, deleteAllProfiles);
 
 router.delete("/profile/:id", authMiddleware, deleteProfileById);
 
-
+router.put("/profile/:id", authMiddleware, updateProfileById);
 
 export default router;
